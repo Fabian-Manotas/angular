417e3c2ae3eff44239f54ed6fc4b083067573300
Francisco jimenez <frankjimenez2707@gmail.com}>
2023-03-23 09:48:25 -0500

    good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component: NavComponent
+  },
+
+  { path:'nav/:variable',
+    component: NavComponent  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
